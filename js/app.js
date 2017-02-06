d9bf65e3aa338cde5c405172ea7d366bd12859db
ellac13 <calle.13@hotmail.com>
2017-02-06 14:58:29 +0100

    Step 4, finished lab 2

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function() {
 	var model = new DinnerModel();
 	
 	//And create the needed controllers and views
-	var exampleView = new ExampleView($("#exampleView"), model);
+	var exampleView = new ExampleView($("#view2"), model);
 	var exampleViewController = new ExampleViewController(exampleView, model);
 
 });
