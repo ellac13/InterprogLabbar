d4feffb10d482590e7bf74096ded84f3ecdad9f9
ellac13 <calle.13@hotmail.com>
2017-02-13 12:25:45 +0100

    View 4 number rounding

--- js/view/view4.js
+++ js/view/view4.js
@@ -22,9 +22,10 @@ var View4 = function (container, model) {
 		var ingredientsHTML = "";
 		var ingredientsPrice = 0;
 		for (var i = 0; i < ingredients.length; i++) {
-			ingredientsHTML = ingredientsHTML + '<tr><td>' + ingredients[i]['quantity'] * numberOfGuests + ' ' + ingredients[i]['unit'] + '</td><td>' + ingredients[i]['name'] + '</td><td>SEK</td><td>' + ingredients[i]['price'] * numberOfGuests + '</td></tr>';
+			ingredientsHTML = ingredientsHTML + '<tr><td>' + parseFloat(ingredients[i]['quantity'] * numberOfGuests).toFixed(1) + ' ' + ingredients[i]['unit'] + '</td><td>' + ingredients[i]['name'] + '</td><td>SEK</td><td>' + ingredients[i]['price'] * numberOfGuests + '</td></tr>';
 			ingredientsPrice += ingredients[i]['quantity'] * ingredients[i]['price'];
 		};
+		ingredientsPrice = parseInt(ingredientsPrice);
 		return '<div class="col-sm-12 col-md-6"><div class="thumbnail"><h2>Ingredients for ' + numberOfGuests +' people</h2><hr><table id="dishIngredientTable">'+ ingredientsHTML +'</table><hr><div><button class="btn btn-default" id="confirmDishButton" type="button">Confirm Dish</button><p style="float:right">Total cost: <span id="dishCost">' + ingredientsPrice * numberOfGuests +'</span> SEK</p></div></div></div>'; 
 		//TODO: Lös knapparna
 	}
